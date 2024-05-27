a24a0c899bb68c6f0671406d04edf47430bb7fd3
FaridaYasserYoussef <68453832+FaridaYasserYoussef@users.noreply.github.com>
2024-05-27 21:37:43 +0300

    heroku deployment requirements

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn drivesafedjango.wsgi:application --log-file -
--- drivesafedjango/settings.py
+++ drivesafedjango/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-h4$_ukt&mair1zeqmub9p6okl6thpes2ymkr@kn5jz#-ewqc8(
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.7.80'
+    'https://drivesafeapp-a301fe33a058.herokuapp.com'
 ]
 
 
@@ -119,8 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
